3e2a0f5b63d5246d08c14a818201b84ab9427f83
JJM van Deursen <janjaap@users.noreply.github.com>
2019-07-23 22:13:46 +0200

    Adds Cypress test and Travis cfg

---
+++ cypress/integration/assert_price.js
@@ -0,0 +1,38 @@
+context("Actions", () => {
+  beforeEach(() => {
+    cy.visit(
+      "https://www.elizawashere.nl/griekenland/lesbos/eftalou_molivos/villas_molivos_castle.htm?view=prices&transport=vl"
+    );
+  });
+
+  describe("Price", () => {
+    it("should be 705", () => {
+      cy.get(".acco-price-block").should("be.visible");
+
+      cy.get(".acco-container.first")
+        .first()
+        .find(".date")
+        .contains("16 sep")
+        .each(element => {
+          const dateEl = element[0];
+          const dateNodes = dateEl.closest(".acco-container").children;
+          const dateElIndex = Array.from(dateNodes).findIndex(node =>
+            Array.from(node.children).includes(dateEl)
+          );
+
+          cy.get(".acco-container .acco")
+            .contains("2 Pers ")
+            .closest(".acco-container")
+            .each(element => {
+              const accoNodes = element[0].children;
+              const accoSingleNode = Array.from(accoNodes)[dateElIndex];
+              const priceStr = accoSingleNode.querySelector(".fact").textContent;
+              const match = priceStr.trim().match(/\d+/);
+              const price = parseInt(match[0], 10);
+
+              expect(price).to.equal(705);
+            });
+        });
+    });
+  });
+});
